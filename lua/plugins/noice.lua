2efd05bd210d86ca37235faf033db3bb923a5c7a
Bolk3 <bolk3@proton.me>
2026-08-17 22:52:00 +0200

    feat(ui): add columnstatus cmdline and bufferline

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+local	categories = require("config.categories")
+
+return {
+	"folke/noice.nvim",
+	enabled = categories.is_enabled("Ui"),
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	}
+}
